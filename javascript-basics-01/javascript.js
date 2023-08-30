e693452dc90c66ef1cfd9ff7da29703551fc0489
alisaghir1 <alisaghir543@gmail.com>
2023-08-30 10:36:13 +0300

    step 6

---
+++ javascript-basics-01/javascript.js
@@ -0,0 +1,5 @@
+const first_number=parseFloat(prompt("input First number"))
+const second_number=parseFloat(prompt("input second number"))
+
+const result=first_number / second_number
+alert(result)
