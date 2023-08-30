3a3f584c4403891ec648a323dbdd9ba8eb9138ce
alisaghir1 <alisaghir543@gmail.com>
2023-08-30 12:14:58 +0300

    step 8

--- javascript-basics-01/javascript.js
+++ javascript-basics-01/javascript.js
@@ -1,8 +1,9 @@
-const num1 = parseFloat(prompt("Enter the first number:"));
-  const num2 = parseFloat(prompt("Enter the second number:"));
 
-  const division= num1 / num2
-  const remainder=num1 % num2
 
-  alert(division)
-  alert(remainder)
+var age = parseInt(prompt("Please enter your age:"));
+
+if (age >= 18) {
+    alert("You are over 18.");
+} else {
+    alert("You are under 18.");
+}
